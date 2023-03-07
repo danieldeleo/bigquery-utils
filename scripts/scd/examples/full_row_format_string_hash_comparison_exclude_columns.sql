ee5f8e9689609f21b7b293d4803b675dfdfc86ef
Daniel De Leo <danieldeleo@users.noreply.github.com>
2023-03-07 09:57:04 -0500

    renaming

---
+++ scripts/scd/examples/full_row_format_string_hash_comparison_exclude_columns.sql
@@ -0,0 +1,32 @@
+-- Create a temp table where one row is changed.
+CREATE TEMP TABLE fstring_row_hashes AS
+SELECT
+  SHA256(FORMAT("%t",(SELECT AS STRUCT t.* EXCEPT(row_number, transactions)))) row_hash,
+FROM(
+  SELECT
+    block_id,
+    IF(block_id = "0000000000000ba1df5fc77872b1225830ccb94d891e0b8643dc8bf140b7e018", "0", previous_block) AS previous_block,
+    * EXCEPT(block_id, previous_block),
+  FROM 
+    `bigquery-public-data.bitcoin_blockchain.blocks`
+) t;
+
+-- Less performant way of finding changed rows
+SELECT 
+  row_hash
+FROM 
+  json_row_hashes
+WHERE row_hash NOT IN(
+  SELECT DISTINCT SHA256(FORMAT("%t",(SELECT AS STRUCT t.* EXCEPT(row_number,transactions)))) row_hash
+  FROM `bigquery-public-data.bitcoin_blockchain.blocks` t
+);
+
+-- More performant way of finding changed rows
+SELECT
+  row_hash
+FROM 
+  json_row_hashes
+EXCEPT DISTINCT(
+  SELECT SHA256(FORMAT("%t",(SELECT AS STRUCT t.* EXCEPT(row_number,transactions)))) row_hash
+  FROM `bigquery-public-data.bitcoin_blockchain.blocks` t
+);
