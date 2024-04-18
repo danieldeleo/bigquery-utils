f472af79fe91780f64d3cbbb247af41a5305eee6
ddeleo <danieldeleo@google.com>
2024-04-18 15:02:23 -0400

    update

--- dataform/examples/bigquery_cicd/terraform/environments/bigquery_cicd/datasets.tf
+++ dataform/examples/bigquery_cicd/terraform/environments/bigquery_cicd/datasets.tf
@@ -2,6 +2,7 @@ module "bigquery-dataset" {
   source     = "github.com/GoogleCloudPlatform/cloud-foundation-fabric//modules/bigquery-dataset?ref=v30.0.0&depth=1"
   project_id = "danny-bq"
   id         = "example_dataset"
+  location   = "US"
   access = {
     owner          = { role = "OWNER", type = "user" }
   }
